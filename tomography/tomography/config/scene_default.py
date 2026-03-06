efceca6a7968a5b55769ee8b9a3cf2ed79b34058
hengzhiyou666 <hengzhiyou666@gmail.com>
2026-03-06 16:34:52 +0800

    调整default参数

--- tomography/tomography/config/scene_default.py
+++ tomography/tomography/config/scene_default.py
@@ -10,17 +10,17 @@ class SceneDefault(Scene):
         self.pcd.file_name = None
 
         # --- 地图与切片 ---
-        self.map.resolution = 0.1   # 平面栅格分辨率 [m/格]，越小越精细、计算量越大
-        self.map.ground_h = 0.0      # 地面高度 z 基准，低于此值的点云会被当作地面处理
-        self.map.slice_dh = 1.5      # 垂直切片高度间隔 [m]，决定 tomogram 层数与每层厚度
+        self.map.resolution = 0.2   # 平面栅格分辨率 [m/格]，越小越精细、计算量越大
+        self.map.ground_h = 0.1      # 地面高度 z 基准，低于此值的点云会被当作地面处理
+        self.map.slice_dh = 1.0      # 垂直切片高度间隔 [m]，决定 tomogram 层数与每层厚度
 
         # --- 可通行性判定 ---
         self.trav.kernel_size = 1   # 局部窗口大小（邻域核边长），用于平滑/统计可通行性
-        self.trav.interval_min = 0.50   # 垂直“可用空间厚度”下限 [m]，低于则判为不可通行
-        self.trav.interval_free = 0.65  # “自由空间”厚度阈值 [m]，高于则视为可通行
-        self.trav.slope_max = 0.45      # 允许的最大坡度（tan），超过判为不可通行
-        self.trav.step_max = 0.3        # 允许的最大台阶/落差 [m]，超过判为不可通行
+        self.trav.interval_min = 0.50   # 垂直“可用空间厚度”下限 [m]，低于则判为不可通行(低于该值，机器狗钻不进去)
+        self.trav.interval_free = 0.65  # “自由空间”厚度阈值 [m]，高于则视为可通行(高于该值，机器狗可以钻进去)
+        self.trav.slope_max = 0.3      # 允许的最大坡度（tan），超过判为不可通行
+        self.trav.step_max = 0.2        # 允许的最大台阶/落差 [m]，超过判为不可通行
         self.trav.standable_ratio = 0.2 # 邻域内“可站立”栅格比例阈值，用于判定是否可停留
         self.trav.cost_barrier = 50.0   # 代价图中的障碍墙值，超过则视为完全不可通行
-        self.trav.safe_margin = 0.4     # 与障碍的安全边距 [m]，规划时留出的缓冲区
-        self.trav.inflation = 0.2       # 障碍膨胀半径 [m]，相当于考虑机器人尺寸的膨胀
+        self.trav.safe_margin = 1.0     # 与障碍的安全边距 [m]，规划时留出的缓冲区
+        self.trav.inflation = 0.5       # 障碍膨胀半径 [m]，相当于考虑机器人尺寸的膨胀
